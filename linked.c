114bd743128e002a2c69e885d2ad58a07197c626
S1h2i3v4a <shivamkeshari990@gmail.com>
2025-01-29 23:12:37 +0530

    first

---
+++ linked.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    int b = 20;
+    printf("hello world");
+    printf("hello world");
+    printf("hello world");
+    printf("hello world");
+    printf("hello world");
+    printf("hello world");
+    printf("%d",a+b);
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Shivam Keshari");
+    return 0;
+}
+//this is a comment
